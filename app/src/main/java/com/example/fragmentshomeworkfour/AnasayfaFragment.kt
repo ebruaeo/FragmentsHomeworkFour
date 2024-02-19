48446a0cd25140b747e9cc53b8c5216b08f2387d
ebruaeo <ebruaeo@hotmail.com>
2024-02-19 21:19:00 +0300

    Anasayfa to A, anasayfa to x

--- app/src/main/java/com/example/fragmentshomeworkfour/AnasayfaFragment.kt
+++ app/src/main/java/com/example/fragmentshomeworkfour/AnasayfaFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.fragmentshomeworkfour.databinding.FragmentAnasayfaBinding
 
 
 class AnasayfaFragment : Fragment() {
-
+    private lateinit var binding: FragmentAnasayfaBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_anasayfa, container, false)
+        binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.goToAPageBtn.setOnClickListener {
+            val action = AnasayfaFragmentDirections.actionAnasayfaFragmentToAFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+        binding.goToXPageBtn.setOnClickListener {
+            val action = AnasayfaFragmentDirections.actionAnasayfaFragmentToXFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
