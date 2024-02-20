939b0caca944be438a5572103acefbc39837d728
ebruaeo <ebruaeo@hotmail.com>
2024-02-20 12:08:10 +0300

    X Page to Y page

--- app/src/main/java/com/example/fragmentshomeworkfour/XFragment.kt
+++ app/src/main/java/com/example/fragmentshomeworkfour/XFragment.kt
@@ -1,18 +1,31 @@
 package com.example.fragmentshomeworkfour
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.fragmentshomeworkfour.databinding.FragmentXBinding
 
 class XFragment : Fragment() {
-
+    private lateinit var binding: FragmentXBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_x, container, false)
+        binding= FragmentXBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.FromXToYBtn.setOnClickListener {
+            val action = XFragmentDirections.actionXFragmentToYFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 
