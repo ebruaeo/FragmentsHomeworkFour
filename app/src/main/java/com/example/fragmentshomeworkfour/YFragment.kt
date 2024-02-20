9062284feb5a2049bf0d984b53be47e59d9606ab
ebruaeo <ebruaeo@hotmail.com>
2024-02-20 12:08:58 +0300

    Y fragment binding

--- app/src/main/java/com/example/fragmentshomeworkfour/YFragment.kt
+++ app/src/main/java/com/example/fragmentshomeworkfour/YFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fragmentshomeworkfour.databinding.FragmentYBinding
 
 class YFragment : Fragment() {
 
+    private lateinit var binding: FragmentYBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_y, container, false)
+        binding=FragmentYBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
