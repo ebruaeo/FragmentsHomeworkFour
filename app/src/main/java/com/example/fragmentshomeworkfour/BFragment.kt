126c3eef629c67465a9dc7501bd051989864afbd
ebruaeo <ebruaeo@hotmail.com>
2024-02-20 12:07:46 +0300

    B Page to Y page

--- app/src/main/java/com/example/fragmentshomeworkfour/BFragment.kt
+++ app/src/main/java/com/example/fragmentshomeworkfour/BFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.fragmentshomeworkfour.databinding.FragmentABinding
+import com.example.fragmentshomeworkfour.databinding.FragmentBBinding
 
 class BFragment : Fragment() {
 
-
+    private lateinit var binding:FragmentBBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b, container, false)
+        binding = FragmentBBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.goToYPageBtn.setOnClickListener {
+            val action = BFragmentDirections.actionBFragmentToYFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 
